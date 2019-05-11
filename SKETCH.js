d9d9a65116508593ec3e48d36d44a0b7a002ee7e
BabylonRocker <48922848+BabylonRocker@users.noreply.github.com>
2019-05-11 14:43:11 -0400

    Add files via upload

--- SKETCH.js
+++ SKETCH.js
@@ -1,6 +1,13 @@
 var yellowRad = 71.406/2
 var yellow1 = 406.203
 
+let myFont;
+
+function preload() {
+  myFont = loadFont('Valera.ttf');
+}
+
+
 function setup() {
     var canvas = createCanvas(windowWidth, 400);
     canvas.parent('header');
@@ -30,7 +37,7 @@ fill(195, 39, 41);
 circle(291.493, 194.749, 106.011/2);
   
 
-  
+textFont(myFont);  
 textSize(30);
 fill(155);
 text("Jackson" ,40,200); //shadow
